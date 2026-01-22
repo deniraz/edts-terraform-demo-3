77a62a56e9fc9b19cda759f3f5fd44f80fca0ef1
deniraz <denidenira007@gmail.com>
2026-01-22 11:17:17 +0700

    demo 3

--- variables.tf
+++ variables.tf
@@ -25,7 +25,7 @@ variable "public_subnet_cidr" {
 variable "instance_type" {
   type        = string
   description = "EC2 instance type."
-  default     = "t3.micro"
+  default     = "t3.small"
 }
 
 variable "ssh_cidr" {
